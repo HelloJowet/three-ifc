3090de499c95c6616b972844a17f4c861dc97c4c
root <jowetmail@gmail.com>
2025-04-11 22:01:48 +0000

    feat: flatbuffers support added

---
+++ src/utils/flatbuffersConverters/geometry.ts
@@ -0,0 +1,23 @@
+import * as flatbuffers from 'flatbuffers'
+
+import { Geometry as FlatbuffersGeometry } from '../../flatbuffers/schema_generated'
+import { Geometry } from '../../graphics'
+
+export class FlatbuffersGeometryConverter {
+  static toFlatbuffersBinary(geometry: Geometry): Uint8Array {
+    const builder = new flatbuffers.Builder()
+
+    const vertices = FlatbuffersGeometry.createVerticesVector(builder, geometry.vertices)
+    const normals = FlatbuffersGeometry.createNormalsVector(builder, geometry.normals)
+    const indices = FlatbuffersGeometry.createIndicesVector(builder, geometry.indices)
+    FlatbuffersGeometry.startGeometry(builder)
+    FlatbuffersGeometry.addVertices(builder, vertices)
+    FlatbuffersGeometry.addNormals(builder, normals)
+    FlatbuffersGeometry.addIndices(builder, indices)
+    const flatbuffersOffset = FlatbuffersGeometry.endGeometry(builder)
+
+    builder.finish(flatbuffersOffset)
+
+    return builder.asUint8Array()
+  }
+}
---
+++ src/utils/flatbuffersConverters/index.ts
@@ -0,0 +1,1 @@
+import { FlatbuffersGeometryConverter } from './geometry'
